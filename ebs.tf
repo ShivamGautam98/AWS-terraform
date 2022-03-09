69bba195e5eafa13e7e4ca6e714a6faf2359034f
ShivamGautam98 <64134683+ShivamGautam98@users.noreply.github.com>
2022-03-10 02:04:37 +0530

    added code for new resources

---
+++ ebs.tf
@@ -0,0 +1,8 @@
+# resource "aws_ebs_volume" "example" {
+#   availability_zone = "ap-south-1a"
+#   size              = 10
+
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
--- iam.tf
+++ iam.tf
@@ -1,22 +1,22 @@
-resource "aws_iam_group_membership" "team" {
-  name = "tf-testing-group-membership"
+# resource "aws_iam_group_membership" "team" {
+#   name = "tf-testing-group-membership"
 
-  users = [
-    aws_iam_user.user_one.name,
-    aws_iam_user.user_two.name,
-  ]
+#   users = [
+#     aws_iam_user.user_one.name,
+#     aws_iam_user.user_two.name,
+#   ]
 
-  group = aws_iam_group.group.name
-}
+#   group = aws_iam_group.group.name
+# }
 
-resource "aws_iam_group" "group" {
-  name = "test-group"
-}
+# resource "aws_iam_group" "group" {
+#   name = "test-group"
+# }
 
-resource "aws_iam_user" "user_one" {
-  name = "test-user"
-}
+# resource "aws_iam_user" "user_one" {
+#   name = "test-user"
+# }
 
-resource "aws_iam_user" "user_two" {
-  name = "test-user-two"
-}
+# resource "aws_iam_user" "user_two" {
+#   name = "test-user-two"
+# }
