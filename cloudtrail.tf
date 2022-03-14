3b0b3d8b9ebe532bf3940f074fab4189cd9582bb
ShivamGautam98 <64134683+ShivamGautam98@users.noreply.github.com>
2022-03-15 00:42:33 +0530

    added cloudtrail

---
+++ cloudtrail.tf
@@ -0,0 +1,4 @@
+# resource "aws_cloudtrail_event_data_store" "example" {
+#   name             = "example-event-data-store"
+#   retention_period = 7
+# }
--- codebuild.tf
+++ codebuild.tf
@@ -1,43 +1,43 @@
-resource "aws_kms_key" "example" {
-  description             = "my test kms key"
-  deletion_window_in_days = 7
+# resource "aws_kms_key" "example" {
+#   description             = "my test kms key"
+#   deletion_window_in_days = 7
 
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "kms-tf-1",
-  "Statement": [
-    {
-      "Sid": "Enable IAM User Permissions",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "kms:*",
-      "Resource": "*"
-    }
-  ]
-}
-POLICY
-}
+#   policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Id": "kms-tf-1",
+#   "Statement": [
+#     {
+#       "Sid": "Enable IAM User Permissions",
+#       "Effect": "Allow",
+#       "Principal": {
+#         "AWS": "*"
+#       },
+#       "Action": "kms:*",
+#       "Resource": "*"
+#     }
+#   ]
+# }
+# POLICY
+# }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "shivam98-test"
-}
+# resource "aws_s3_bucket" "example" {
+#   bucket = "shivam98-test"
+# }
 
-resource "aws_codebuild_report_group" "example" {
-  name = "shivam98-group"
-  type = "TEST"
+# resource "aws_codebuild_report_group" "example" {
+#   name = "shivam98-group"
+#   type = "TEST"
 
-  export_config {
-    type = "S3"
+#   export_config {
+#     type = "S3"
 
-    s3_destination {
-      bucket              = aws_s3_bucket.example.id
-      encryption_disabled = false
-      encryption_key      = aws_kms_key.example.arn
-      packaging           = "NONE"
-      path                = "/some"
-    }
-  }
-}
+#     s3_destination {
+#       bucket              = aws_s3_bucket.example.id
+#       encryption_disabled = false
+#       encryption_key      = aws_kms_key.example.arn
+#       packaging           = "NONE"
+#       path                = "/some"
+#     }
+#   }
+# }
