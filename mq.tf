237018158388ad936c5b8d944bcead33f596505e
ShivamGautam98 <64134683+ShivamGautam98@users.noreply.github.com>
2022-03-24 17:19:30 +0530

    added new services

---
+++ mq.tf
@@ -0,0 +1,13 @@
+# resource "aws_mq_broker" "example" {
+#   broker_name = "example"
+
+
+#   engine_type        = "ActiveMQ"
+#   engine_version     = "5.15.9"
+#   host_instance_type = "mq.t2.micro"
+
+#   user {
+#     username = "ExampleUser"
+#     password = "MindTheGap!stayAway100ft"
+#   }
+# }
